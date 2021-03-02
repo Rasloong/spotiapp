61e2b4bb7acf0d93580ebad7a289853e7c815f13
Rasloong <jorlu.arles@gmail.com>
2021-03-02 16:01:17 -0500

    optimizacion

--- src/app/services/spotifty.service.ts
+++ src/app/services/spotifty.service.ts
@@ -10,33 +10,27 @@ export class SpotiftyService {
   constructor(private http: HttpClient) {
     console.log('servicio spotify corriendo');
   }
-  getNewReleases() {
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQCs3IxyTP1k2lo66iLOTiCwjChvQsUAgM4xappO1W04xcWkp3J-dSsbXhXFMLy5l2eIFSLcnC1T4IboovE',
+        'Bearer BQCsvGuMSX69k2z8VjaOas3fxOzbE-Gw4Mb8J42niTMskn5AgkgCHpe5GS3fKjeC3J83w4SFJuVcgyhR3Qw'
     });
-
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases?country=CO', {
-        headers,
+    return this.http.get(url,{headers});
+  }
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?country=CO')
+    .pipe(map((data) => {
+        return data['albums'].items;
       })
-      .pipe(
-        map((data) => {
-          return data['albums'].items;
-        })
-      );
+    );
   }
   getArtist(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQCs3IxyTP1k2lo66iLOTiCwjChvQsUAgM4xappO1W04xcWkp3J-dSsbXhXFMLy5l2eIFSLcnC1T4IboovE',
-    });
-
-    return this.http.get(
-      `https://api.spotify.com/v1/search?q=${termino}&type=artist`,
-      {
-        headers,
-      }
-    ).pipe(map((data)=>{return data['artists'].items}));
+    return this.getQuery(`search?q=${termino}&type=artist`)
+      .pipe(map((data) => {
+        return data['artists'].items;
+      })
+    );
   }
 }
